9f4187b2dbe75fa1ff5a94f53dc56851a8097fd0
NooraAl70sani <160010781+NooraAl70sani@users.noreply.github.com>
2024-03-29 12:44:55 +0400

    Exhibition.py

--- Exhibition.py
+++ Exhibition.py
@@ -6,3 +6,21 @@ class Exhibition:
 
     def displayInfo(self):
         print(f"Name: {self.name}, Duration: {self.duration}, Location: {self.location.value}")
+
+    def setName(self, name):
+        self.name = name
+
+    def setDuration(self, duration):
+        self.duration = duration
+
+    def setLocation(self, location):
+        self.location = location
+
+    def getName(self):
+        return self.name
+
+    def getDuration(self):
+        return self.duration
+
+    def getLocation(self):
+        return self.location
