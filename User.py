b4e3bd96f09413b32d75974f7bb1859cc84ba322
NooraAl70sani <160010781+NooraAl70sani@users.noreply.github.com>
2024-03-29 11:20:43 +0400

    User.py

--- User.py
+++ User.py
@@ -4,3 +4,28 @@ class User:
         self.password = password
         self.artworks = []
         self.tickets = []
+
+    def setUsername(self, username):
+        self.username = username
+
+    def setPassword(self, password):
+        self.password = password
+
+    def setArtworks(self, artworks):
+        self.artworks = artworks
+
+    def setTickets(self, tickets):
+        self.tickets = tickets
+
+    def getUsername(self):
+        return self.username
+
+    def getPassword(self):
+        return self.password
+
+    def getArtworks(self):
+        return self.artworks
+
+    def getTickets(self):
+        return self.tickets
+
