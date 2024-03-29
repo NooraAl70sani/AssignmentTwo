c9471037c23304f27d8fc7e286bba83b2617fb73
NooraAl70sani <160010781+NooraAl70sani@users.noreply.github.com>
2024-03-29 12:45:54 +0400

    Event.py

--- Event.py
+++ Event.py
@@ -7,3 +7,27 @@ class Event:
 
     def displayInfo(self):
         print(f"Name: {self.name}, Location: {self.location.value}, Duration: {self.duration}, Price: {self.price}")
+
+    def setName(self, name):
+        self.name = name
+
+    def setLocation(self, location):
+        self.location = location
+
+    def setDuration(self, duration):
+        self.duration = duration
+
+    def setPrice(self, price):
+        self.price = price
+
+    def getName(self):
+        return self.name
+
+    def getLocation(self):
+        return self.location
+
+    def getDuration(self):
+        return self.duration
+
+    def getPrice(self):
+        return self.price
